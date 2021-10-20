0cde6906f4587f69458ee23905e3f9a49395c988
Vladosbandosss <dasistperfektos@gmail.com>
2021-10-20 18:28:35 +0300

    Prototype1
    прототип

---
+++ Scripts/MoveMent.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMent : MonoBehaviour
+{
+    Rigidbody rb;
+    AudioSource aus;
+    [SerializeField] float mainThrust = 800f;
+
+    [SerializeField] float mainRotate = 100f;
+
+
+    [SerializeField] AudioClip mainEngine;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        aus = GetComponent<AudioSource>();
+    }
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    
+
+    private void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up*mainThrust*Time.deltaTime);
+            if (!aus.isPlaying)
+            {
+                aus.PlayOneShot(mainEngine);
+            }
+           
+        }
+        else
+        {
+            aus.Stop();
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward*Time.deltaTime*mainRotate);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * Time.deltaTime * mainRotate);
+        }
+    }
+}
---
+++ Scripts/PlayerColl.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerColl : MonoBehaviour
+{
+    [SerializeField] AudioClip succes;
+    [SerializeField] AudioClip crash;
+
+    AudioSource aus;
+
+    [SerializeField] ParticleSystem explose;
+    [SerializeField] ParticleSystem winSucces;
+
+    private void Awake()
+    {
+        aus = GetComponent<AudioSource>();
+    }
+    private void OnCollisionEnter(Collision coll)
+    {
+        switch (coll.gameObject.tag)
+        {
+            case "Fuel":
+                Debug.Log("топливо");
+                break;
+
+            case "Finish":
+                winSucces.Play();
+                aus.PlayOneShot(succes);
+                LoadNextLevel();
+              
+                break;
+
+            case "Friendly":
+                Debug.Log("дружеств платворма");
+                break;
+
+            default:
+                explose.Play();
+                aus.PlayOneShot(crash);
+                Invoke(nameof(RelodLevel), 1f);
+              
+                break;
+
+        }
+    }
+
+    private void RelodLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void LoadNextLevel()
+    {
+        int indexToload = SceneManager.GetActiveScene().buildIndex;
+        indexToload++;
+        if (indexToload == SceneManager.sceneCountInBuildSettings)
+        {
+            indexToload = 0;
+        }
+        SceneManager.LoadScene(indexToload);
+    }
+}
